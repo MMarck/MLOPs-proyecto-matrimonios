d354ddb677a9c09e2c80e2730e4df989d287145a
maanmuri <maanmuri@github.com>
2025-06-23 23:42:30 -0500

    se modifica parametros del modelo inicial para entrenar con menos recursos

--- airflow/dags/step_two_entrenamiento_divorcios.py
+++ airflow/dags/step_two_entrenamiento_divorcios.py
@@ -55,11 +55,11 @@ def dag_reentrenamiento_modelo():
 
         def load_the_initial_model():
 
-            model_data = RandomForestClassifier( n_estimators=700,
+            model_data = RandomForestClassifier( n_estimators=100,
                                         min_samples_split=10,
                                         min_samples_leaf=2,
                                         max_features='sqrt',
-                                        max_depth=30,
+                                        max_depth=10,
                                         bootstrap=False,
                                         random_state=42)
             
@@ -115,15 +115,14 @@ def dag_reentrenamiento_modelo():
             os.remove(signature_path)
 
             # no me es posible guardar el modelo completo sin que muera el dag
-            # signature = infer_signature(X, model.predict(X))
-            # mlflow.sklearn.log_model(
-            #     sk_model=model,
-            #     artifact_path=artifact_path,
-            #     signature=signature,
-            #     serialization_format='pickle',
-            #     registered_model_name="divorcios_ecuador_model_dev",
-            #     metadata={"model_data_version": 1}
-            # )
+            mlflow.sklearn.log_model(
+                sk_model=model,
+                artifact_path=artifact_path,
+                signature=signature,
+                serialization_format='pickle',
+                registered_model_name="divorcios_ecuador_model_dev",
+                metadata={"model_data_version": 1}
+            )
 
             # Obtain the model URI
             return mlflow.get_artifact_uri(artifact_path)
@@ -154,7 +153,7 @@ def dag_reentrenamiento_modelo():
             )
 
             # Save the alias as challenger
-            client.set_registered_model_alias(name, "challenger", result.version)
+            client.set_registered_model_alias(name, "champion", result.version)
 
         # Load the champion model
         initial_model = load_the_initial_model()
@@ -174,100 +173,9 @@ def dag_reentrenamiento_modelo():
         artifact_uri = mlflow_track_experiment(initial_model, X_train)
 
         # Record the model
-        # register_challenger(initial_model, f1_score, artifact_uri)
+        register_challenger(initial_model, f1_score, artifact_uri)
 
 
-    @task.virtualenv(
-        task_id="train_the_challenger_model",
-        requirements=["scikit-learn==1.3.2",
-                      "mlflow==2.10.2",
-                      "awswrangler==3.6.0"],
-        system_site_packages=True
-    )
-    def evaluate_champion_challenge():
-        import mlflow
-        import awswrangler as wr
-
-        from sklearn.metrics import f1_score
-
-        mlflow.set_tracking_uri('http://mlflow:5000')
-
-        def load_the_model(alias):
-            model_name = "heart_disease_model_prod"
-
-            client = mlflow.MlflowClient()
-            model_data = client.get_model_version_by_alias(model_name, alias)
-
-            model = mlflow.sklearn.load_model(model_data.source)
-
-            return model
-
-        def load_the_test_data():
-            X_test = wr.s3.read_csv("s3://data/final/test/heart_X_test.csv")
-            y_test = wr.s3.read_csv("s3://data/final/test/heart_y_test.csv")
-
-            return X_test, y_test
-
-        def promote_challenger(name):
-
-            client = mlflow.MlflowClient()
-
-            # Demote the champion
-            client.delete_registered_model_alias(name, "champion")
-
-            # Load the challenger from registry
-            challenger_version = client.get_model_version_by_alias(name, "challenger")
-
-            # delete the alias of challenger
-            client.delete_registered_model_alias(name, "challenger")
-
-            # Transform in champion
-            client.set_registered_model_alias(name, "champion", challenger_version.version)
-
-        def demote_challenger(name):
-
-            client = mlflow.MlflowClient()
-
-            # delete the alias of challenger
-            client.delete_registered_model_alias(name, "challenger")
-
-        # Load the champion model
-        champion_model = load_the_model("champion")
-
-        # Load the challenger model
-        challenger_model = load_the_model("challenger")
-
-        # Load the dataset
-        X_test, y_test = load_the_test_data()
-
-        # Obtain the metric of the models
-        y_pred_champion = champion_model.predict(X_test)
-        f1_score_champion = f1_score(y_test.to_numpy().ravel(), y_pred_champion)
-
-        y_pred_challenger = challenger_model.predict(X_test)
-        f1_score_challenger = f1_score(y_test.to_numpy().ravel(), y_pred_challenger)
-
-        experiment = mlflow.set_experiment("Heart Disease")
-
-        # Obtain the last experiment run_id to log the new information
-        list_run = mlflow.search_runs([experiment.experiment_id], output_format="list")
-
-        with mlflow.start_run(run_id=list_run[0].info.run_id):
-            mlflow.log_metric("test_f1_challenger", f1_score_challenger)
-            mlflow.log_metric("test_f1_champion", f1_score_champion)
-
-            if f1_score_challenger > f1_score_champion:
-                mlflow.log_param("Winner", 'Challenger')
-            else:
-                mlflow.log_param("Winner", 'Champion')
-
-        name = "heart_disease_model_prod"
-        if f1_score_challenger > f1_score_champion:
-            promote_challenger(name)
-        else:
-            demote_challenger(name)
-
-    # train_the_challenger_model() >> evaluate_champion_challenge()
     train_the_challenger_model() 
 
 
